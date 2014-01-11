3cbfb8d1eb95aed4db97cab85607930a877b71e6
Raghav Karol <rk@issuu.com>
2014-01-11 23:24:15 +0100

    add helper script to run on change

--- scripts/run_on_change.sh
+++ scripts/run_on_change.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+# Run a command when a watched file changes.
+#
+# $1 is a command that generates a list of file paths to watched
+# $2 is the command to run when a file on the watch paths changed
+#
+# Examples:
+#
+#   run_on_change.sh 'find . -name "*.erl"' 'make compile test'
+#    run compile and test every time a erl file changes
 
+#   run_on_change.sh 'echo arch.dot' 'dot -Tpng -o arch.png arch.dot'
+#    create arch.png every time the dot file changes
+
+#!/bin/bash
 POLL_TIME_SECS=1
 FIND_WATCHED_FILES=$1
 CMD=$2
